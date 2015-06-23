50f31405262115cbdf63f3b78ce8a76fcdd26731
Pascal M <mail@mkpascal.net>
2015-06-23 15:48:06 -0700

    Initial Commit

---
+++ imsgbridge.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'sinatra/jsonp'
+require 'sinatra/config_file'
+
+set :raise_errors, false
+set :show_exceptions, false
+
+config_file 'config.yml'
+
+get '/imsgbridge' do
+  to = params['to']
+  message = params['message']
+  password_params = params['password']
+  password = settings.password
+
+  if password_params == password
+    system 'osascript <<EOF
+        tell application "Messages"
+                set targetService to 1st service whose service type = iMessage
+                set targetBuddy to buddy "' + to + '" of targetService
+                send "' + message + '" to targetBuddy
+        end tell
+	EOF'
+
+    data = {:status => 'sent'}
+  else
+    data = {:status => 'unauthenticated'}
+  end
+
+  JSONP data
+end
+
+error do
+  data = {:status => 'error'}
+  JSONP data
+end
